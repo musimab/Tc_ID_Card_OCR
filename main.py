11a74d902d1453e33e8e92a76f43969312b49fbc
musimab <mustafabuyuk42@hotmail.com>
2022-03-01 21:23:51 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,325 @@
+import cv2
+from matplotlib import pyplot as plt
+import pytesseract
+import numpy as np
+import utlis
+
+
+def ocrOutputs(img, bbox):
+    
+    LowerThr = 7
+    UpperThr = 7
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    for box in bbox :
+        x, w, y, h = box
+        crop_img = img_rgb[y-LowerThr:y+h+UpperThr, x-LowerThr:x+w+UpperThr]
+        processed_img = utlis.denoiseImage(crop_img)
+        cv2.imshow("crop img", processed_img)
+        cv2.waitKey(0)
+        print(pytesseract.image_to_string(processed_img))
+
+
+def getCenterRatios(img, centers):
+
+    if(len(img.shape) == 2):
+        img_h, img_w = img.shape
+        ratios = np.zeros_like(centers, dtype=np.float32)
+        for i, center in enumerate(centers):
+            ratios[i] = (center[0]/img_w, center[1]/img_h)
+        return ratios
+    else :
+        img_h, img_w,_ = img.shape
+        ratios = np.zeros_like(centers, dtype=np.float32)
+        for i, center in enumerate(centers):
+            ratios[i] = (center[0]/img_w, center[1]/img_h)
+        return ratios
+
+
+def displayMachedBoxes(img, new_bboxes):
+    
+    for box in new_bboxes:
+        x1, w, y1, h = box
+        cv2.rectangle(img, (x1, y1), (x1+w, y1+h), (255,0,0), 2)
+        cX = round(int(x1) + w/2.0)
+        cY = round(int(y1) + h/2.0)
+        cv2.circle(img, (cX, cY), 7, (0, 255, 0), -1)
+        
+    return img
+
+
+def matchCenters(ratios1, ratios2):
+
+    bbb0 = np.zeros_like(ratios2)
+    bbb1 = np.zeros_like(ratios2)
+    bbb2 = np.zeros_like(ratios2)
+    bbb3 = np.zeros_like(ratios2)
+
+    for i , r2 in enumerate(ratios2):
+        bbb0[i] = abs(ratios1[0] - r2)
+        bbb1[i] = abs(ratios1[1] - r2)
+        bbb2[i] = abs(ratios1[2] - r2)
+        bbb3[i] = abs(ratios1[3] - r2)
+
+    sum_b0 = np.sum(bbb0, axis = 1)
+    sum_b0 = np.reshape(sum_b0, (-1, 1))
+    arg_min_b0 = np.argmin(sum_b0, axis=0)
+
+    sum_b1 = np.sum(bbb1, axis = 1)
+    sum_b1 = np.reshape(sum_b1, (-1, 1))
+    arg_min_b1 = np.argmin(sum_b1, axis=0)
+
+    sum_b2 = np.sum(bbb2, axis = 1)
+    sum_b2 = np.reshape(sum_b2, (-1, 1))
+    arg_min_b2 = np.argmin(sum_b2, axis=0)
+
+    sum_b3 = np.sum(bbb3, axis = 1)
+    sum_b3 = np.reshape(sum_b3, (-1, 1))
+    arg_min_b3 = np.argmin(sum_b3, axis=0)
+
+    return np.squeeze(arg_min_b0), np.squeeze(arg_min_b1), np.squeeze(arg_min_b2),np.squeeze(arg_min_b3)         
+
+
+def getCenterOfMasks(thresh):
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detected_centers = []
+    img_h, img_w = thresh.shape
+
+    for contour in contours:
+        (x,y,w,h) = cv2.boundingRect(contour)
+        M = cv2.moments(contour)
+        cX = round(int(x) + w/2.0)
+        cY = round(int(y) + h/2.0)
+        detected_centers.append((cX, cY))
+        cv2.circle(thresh, (cX, cY), 7, (0, 0, 0), -1)
+        if w > 10 and h > 10:
+            cv2.rectangle(thresh, (x,y), (x+w,y+h), (255, 0, 0), 2)
+    
+    return np.array(detected_centers)
+
+
+def getExtendedBoxCoordinates(box1, box1_r):
+    
+    DISTANCE_THRES = 10
+    new_box = np.zeros_like(box1)
+    new_box[0] = box1[0] if(box1[0] < box1_r[0]) else box1_r[0]
+    new_box[1] = box1_r[1] + box1[1]  + DISTANCE_THRES 
+    new_box[2] = box1[2]
+    new_box[3] = box1[3] if(box1[3] > box1_r[3]) else box1_r[3]
+    return new_box
+
+
+def searchNearestBoundingBoxes(box_coordinates, box_indexes, img):
+    
+    DISTANCE_THRES = 10
+    
+    right_centers, left_centers, right_centers_box_full, left_centers_box_full = utlis.getRightAndLeftBoxCenters(box_coordinates, box_indexes)
+    
+    box1 = box_coordinates[box_indexes[0]]
+    box2 = box_coordinates[box_indexes[1]]
+    box3 = box_coordinates[box_indexes[2]]
+    box4 = box_coordinates[box_indexes[3]]
+
+    right_centers_distance1 = np.zeros((len(right_centers_box_full), 1))
+    right_centers_distance2 = np.zeros((len(right_centers_box_full), 1))
+    right_centers_distance3 = np.zeros((len(right_centers_box_full), 1))
+    right_centers_distance4 = np.zeros((len(right_centers_box_full), 1))
+
+    left_centers_distance1 = np.zeros((len( left_centers_box_full), 1) )
+    left_centers_distance2 = np.zeros((len( left_centers_box_full), 1) )
+    left_centers_distance3 = np.zeros((len( left_centers_box_full), 1) )
+    left_centers_distance4 = np.zeros((len( left_centers_box_full), 1) )
+
+
+    for i , left_box_centers in enumerate(left_centers_box_full):
+        
+        right_centers_distance1[i] = np.linalg.norm(right_centers[0] - left_box_centers) #box1 right center - other boxes left center
+        right_centers_distance2[i] = np.linalg.norm(right_centers[1] - left_box_centers)
+        right_centers_distance3[i] = np.linalg.norm(right_centers[2] - left_box_centers)
+        right_centers_distance4[i] = np.linalg.norm(right_centers[3] - left_box_centers)
+    
+    for i , right_box_centers in enumerate(right_centers_box_full):
+        
+        left_centers_distance1[i] = np.linalg.norm(left_centers[0] - right_box_centers) # box1 left center - other boexes right center
+        left_centers_distance2[i] = np.linalg.norm(left_centers[1] - right_box_centers)
+        left_centers_distance3[i] = np.linalg.norm(left_centers[2] - right_box_centers)
+        left_centers_distance4[i] = np.linalg.norm(left_centers[3] - right_box_centers)
+
+    box1_r_neighbours = np.where(np.all(right_centers_distance1>0, axis=1 ) & np.all(right_centers_distance1 < [DISTANCE_THRES], axis=1))
+    box2_r_neighbours = np.where(np.all(right_centers_distance2>0, axis=1 ) & np.all(right_centers_distance2 < [DISTANCE_THRES], axis=1))
+    box3_r_neighbours = np.where(np.all(right_centers_distance3>0, axis=1 ) & np.all(right_centers_distance3 < [DISTANCE_THRES], axis=1))
+    box4_r_neighbours = np.where(np.all(right_centers_distance4>0, axis=1 ) & np.all(right_centers_distance4 < [DISTANCE_THRES], axis=1))
+
+    if(box1_r_neighbours[0].size):
+        box_index = 0
+        box1_r_indexes = np.squeeze(box1_r_neighbours)
+        box1_r = box_coordinates[box1_r_indexes]
+        new_box1 = getExtendedBoxCoordinates(box1, box1_r)
+
+        print("box1:", box1)
+        print("right box1:", box1_r)
+        print("new box1:", new_box1)
+        img = utlis.drawlineBetweenBox(box_index, right_centers, left_centers_box_full, box1_r_neighbours, img)
+        box1 = new_box1
+    
+    if(box2_r_neighbours[0].size):
+        box_index = 1
+        box2_r_indexes = np.squeeze(box2_r_neighbours)
+        box2_r = box_coordinates[box2_r_indexes]
+        new_box2 = getExtendedBoxCoordinates(box2, box2_r)
+     
+        print("box2:", box2)
+        print("right box2:",box2_r)
+        print("new box2:", new_box2)
+        img = utlis.drawlineBetweenBox(box_index, right_centers, left_centers_box_full, box2_r_neighbours, img)
+        box2 = new_box2
+    
+    if(box3_r_neighbours[0].size):
+        box_index = 2
+        box3_r_indexes = np.squeeze(box3_r_neighbours)
+        box3_r = box_coordinates[box3_r_indexes]
+        new_box3 = getExtendedBoxCoordinates(box3, box3_r)
+
+        print("box3:", box3)
+        print("right box3:",box3_r)
+        print("new box3:", new_box3)
+        img = utlis.drawlineBetweenBox(box_index, right_centers, left_centers_box_full, box3_r_neighbours, img)
+        box3 = new_box3
+
+    if(box4_r_neighbours[0].size):
+        box_index = 3
+        box4_r_indexes = np.squeeze(box4_r_neighbours)
+        box4_r = box_coordinates[box4_r_indexes]
+        new_box4 = getExtendedBoxCoordinates(box4, box4_r)
+
+        print("box4:", box4)
+        print("right box4:",box4_r)
+        print("new box4:", new_box4)
+        img = utlis.drawlineBetweenBox(box_index, right_centers, left_centers_box_full, box4_r_neighbours, img)
+        box4 = new_box4
+
+    box1_l_neighbours = np.where(np.all(left_centers_distance1>0, axis=1 ) & np.all(left_centers_distance1 < [DISTANCE_THRES], axis=1))
+    box2_l_neighbours = np.where(np.all(left_centers_distance2>0, axis=1 ) & np.all(left_centers_distance2 < [DISTANCE_THRES], axis=1))
+    box3_l_neighbours = np.where(np.all(left_centers_distance3>0, axis=1 ) & np.all(left_centers_distance3 < [DISTANCE_THRES], axis=1))
+    box4_l_neighbours = np.where(np.all(left_centers_distance4>0, axis=1 ) & np.all(left_centers_distance4 < [DISTANCE_THRES], axis=1))
+
+    if(box1_l_neighbours[0].size):
+        
+        box_index = 0
+        box1_l_indexes = np.squeeze(box1_l_neighbours)
+        box1_l = box_coordinates[box1_l_indexes]
+        new_box1 = getExtendedBoxCoordinates(box1, box1_l)
+        
+        print("box1:", box1)
+        print("left box1:", box1_l)
+        print("new box1:", new_box1)
+        img = utlis.drawlineBetweenBox(box_index, left_centers, right_centers_box_full, box1_l_neighbours, img)
+        box1 = new_box1
+
+    if(box2_l_neighbours[0].size):
+        
+        box_index = 1
+        box2_l_indexes = np.squeeze(box2_l_neighbours)
+        box2_l = box_coordinates[box2_l_indexes]
+        new_box2 = getExtendedBoxCoordinates(box2, box2_l)
+        print("box2:", box2)
+        print("left box2:", box1_l)
+        print("new box2:", new_box2)
+        img = utlis.drawlineBetweenBox(box_index, left_centers, right_centers_box_full, box2_l_neighbours, img)
+        box2 = new_box2
+
+    if(box3_l_neighbours[0].size):
+        
+        box_index = 2
+        box3_l_indexes = np.squeeze(box3_l_neighbours)
+        box3_l = box_coordinates[box3_l_indexes]
+        new_box3 = getExtendedBoxCoordinates(box3, box3_l)
+        print("box3:", box3)
+        print("left box3:", box3_l)
+        print("new box3:", new_box3)
+        img = utlis.drawlineBetweenBox(box_index, left_centers, right_centers_box_full, box3_l_neighbours, img)
+        box3 = new_box3
+
+    if(box4_l_neighbours[0].size):
+        
+        box_index = 3
+        box4_l_indexes = np.squeeze(box4_l_neighbours)
+        box4_l = box_coordinates[box4_l_indexes]
+        new_box1 = getExtendedBoxCoordinates(box4, box4_l)
+        print("box4:", box4)
+        print("left box4:", box4_l)
+        print("new box4:", new_box4)
+        img = utlis.drawlineBetweenBox(box_index, left_centers, right_centers_box_full, box4_l_neighbours, img)
+        box4 = new_box4
+
+    return box1, box2, box3, box4
+
+
+def changeOrientationUntilFaceFound(image):
+    
+    img = image.copy()
+    face_conf = []
+    
+    for angle in range(0,360, 15):
+        rotated_img = utlis.rotate_bound(img.copy(), angle)
+        face_conf.append((utlis.detectFace(rotated_img), angle))
+
+    face_confidence = np.array(face_conf)
+    face_arg_max = np.argmax(face_confidence, axis=0)
+    angle_max = face_confidence[face_arg_max[0]][1]
+    print("Maximum face confidence score at angle: ", angle_max)
+    rotated_img = utlis.rotate_bound(image, angle_max)
+    
+    return rotated_img
+
+
+if '__main__' == __name__:
+    
+    img1 = cv2.imread("images/id1.jpeg")
+    txt_path = "BoxCoordinates/res_final_img.txt"
+    
+    plt.title("Original image")
+    plt.imshow(img1)
+    plt.show()
+    
+    img1 = changeOrientationUntilFaceFound(img1)
+    
+    final_img = utlis.correctPerspective(img1)
+    
+    bbox_coordinates , centers = utlis.readBBoxCordinatesAndCenters(txt_path)
+    
+    img1_mask = cv2.imread("mask/res_final_img_mask_.jpg")
+    
+    mask_gray = cv2.cvtColor(img1_mask, cv2.COLOR_BGR2GRAY)
+    
+    ret, binary_mask = cv2.threshold(mask_gray, 127, 255, cv2.THRESH_BINARY)
+    
+    detected_centers = getCenterOfMasks(binary_mask)
+   
+    # centers ratio for 4 boxes
+    centers_ratio_mask = getCenterRatios(binary_mask, detected_centers) 
+    
+    # centers ratio for all boxes
+    centers_ratio_all = getCenterRatios(final_img, centers) 
+  
+    matched_box_indexes = matchCenters(centers_ratio_mask , centers_ratio_all)
+
+    new_bboxes = searchNearestBoundingBoxes(bbox_coordinates, matched_box_indexes, final_img.copy() )
+    
+    ocrOutputs(final_img, new_bboxes) 
+    
+    displayMachedBoxes(final_img, new_bboxes)
+    
+    #utlis.displayAllBoxes(final_img, bbox_coordinates)
+    
+    plt.figure()
+    plt.title("Target box")
+    plt.imshow(final_img)
+    plt.figure()
+    plt.title("Mask")
+    plt.imshow(binary_mask )
+    #plt.figure()
+    #plt.imshow(img_res)
+    plt.show()
+   
+        
+
---
+++ utlis.py
@@ -0,0 +1,238 @@
+import cv2
+from matplotlib import pyplot as plt
+import pytesseract
+import numpy as np
+
+modelFile = "model/res10_300x300_ssd_iter_140000.caffemodel"
+configFile = "model/deploy.prototxt.txt"
+FaceNet = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+
+
+def readBBoxCordinatesAndCenters(coordinates_txt):
+    boxes = []
+    centers = []
+    with open(coordinates_txt,"r+") as file:
+        for line in file:
+            x1,y1, x2, y2, x3, y3, x4, y4 = np.int0(line.split(','))
+
+            x = min(x1, x3)
+            y = min(y1, y2)
+            w = abs(min(x1,x3) - max(x2, x4))
+            h = abs(min(y1,y2) - max(y3, y4))
+
+            cX = round(int(x) + w/2.0)
+            cY = round(int(y) + h/2.0)
+            centers.append((cX, cY))
+            bbox = (int(x), w, int(y), h)
+            boxes.append(bbox)
+    print("number of boxes", len(boxes))
+    return np.array(boxes), np.array(centers)
+
+
+def correctPerspective(img):
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(gray, (5,5), 1)
+    imgCanny = cv2.Canny(imgBlur,10,100)
+    #ret, thresh  = cv2.threshold(gray , 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    #thresh = cv2.adaptiveThreshold( gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    
+    kernel = np.ones((5,5), np.uint8)
+    img_dilation = cv2.dilate( imgCanny, kernel, iterations=2)
+    img_erosion = cv2.erode(img_dilation , kernel, iterations=1)
+
+    cntrs ,hiarchy = cv2.findContours(img_erosion , cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    areas = [cv2.contourArea(c) for c in cntrs]
+    max_index = np.argmax(areas)
+    cnt = cntrs[max_index]
+    x,y,w,h = cv2.boundingRect(cnt)
+
+    #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+    #c = max(cntrs, key = cv2.contourArea)
+    #x,y,w,h = cv2.boundingRect(c)
+    #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),5)
+    
+    rotrect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rotrect)
+    box = np.int0(box)
+  
+    angle = rotrect[-1]
+
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    #print("Angle:", angle)
+    (heigth_q, width_q) = img.shape[:2]
+    (cx, cy) = (width_q // 2, heigth_q // 2)
+    
+    #rotated_img = rotate_bound(img, angle)
+    #new_bbox = rotate_bbox(box, cx, cy,  heigth_q, width_q, angle)
+    warped_img = warpImg(img, box,  width_q, heigth_q)
+    
+    plt.title("rotated image")
+    plt.imshow(img)
+    plt.show()
+
+    
+    plt.title("processed image")
+    plt.imshow(img_erosion)
+    plt.show()
+
+    plt.title("warped image")
+    plt.imshow(warped_img)
+    plt.show()
+    #cv2.imwrite("warped_img.jpg", warped_img)
+
+
+    return warped_img
+
+def reorder(myPoints):
+
+    myPointsNew = np.zeros_like(myPoints)
+    myPoints = myPoints.reshape((4,2))
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+
+    diff = np.diff(myPoints, axis = 1)
+    
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    return myPointsNew
+
+
+def warpImg(img, points, w, h):
+
+    points = reorder(points)
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0,0], [w,0], [0,h], [w,h]])
+    matrix =  cv2.getPerspectiveTransform(pts1, pts2)
+    imgWarp = cv2.warpPerspective(img, matrix, (w,h))
+
+    return imgWarp
+
+def rotate_bound(image, angle):
+    # grab the dimensions of the image and then determine the
+    # centre
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH))
+
+def rotate_bbox(bb, cx, cy, h, w, theta):
+    
+    new_bb = np.zeros_like(bb)
+    for i,coord in enumerate(bb):
+        # opencv calculates standard transformation matrix
+        M = cv2.getRotationMatrix2D((cx, cy), theta, 1.0)
+        # Grab  the rotation components of the matrix)
+        cos = np.abs(M[0, 0])
+        sin = np.abs(M[0, 1])
+        # compute the new bounding dimensions of the image
+        nW = int((h * sin) + (w * cos))
+        nH = int((h * cos) + (w * sin))
+        # adjust the rotation matrix to take into account translation
+        M[0, 2] += (nW / 2) - cx
+        M[1, 2] += (nH / 2) - cy
+        # Prepare the vector to be transformed
+        v = [coord[0],coord[1],1]
+        # Perform the actual rotation and return the image
+        calculated = np.dot(M,v)
+        new_bb[i] = (calculated[0],calculated[1])
+    
+    return new_bb
+
+def displayAllBoxes(img, rect):
+    
+    for rct in rect:
+        x1, w, y1, h = rct
+        cv2.rectangle(img, (x1, y1), (x1+w, y1+h), (255,0,0), 2)
+        cX = round(int(x1) + w/2.0)
+        cY = round(int(y1) + h/2.0)
+        cv2.circle(img, (cX, cY), 7, (255, 0, 0), -1)
+    
+    return img
+
+def getRightAndLeftBoxCenters(box_coordinates, box_indexes):
+    
+    right_centers = np.zeros((4,2), dtype=np.int32)
+    left_centers = np.zeros((4,2), dtype=np.int32)
+    
+    right_centers_box_full = np.zeros((len(box_coordinates),2))
+    left_centers_box_full  = np.zeros((len(box_coordinates),2))
+
+    box1 = box_coordinates[box_indexes[0]]
+    box2 = box_coordinates[box_indexes[1]]
+    box3 = box_coordinates[box_indexes[2]]
+    box4 = box_coordinates[box_indexes[3]]
+ 
+    right_centers[0] = (box1[0]+ box1[1], round(box1[2]+box1[3]/2))
+    right_centers[1] = (box2[0]+ box2[1], round(box2[2]+box2[3]/2))
+    right_centers[2] = (box3[0]+ box3[1], round(box3[2]+box3[3]/2))
+    right_centers[3] = (box4[0]+ box4[1], round(box4[2]+box4[3]/2))
+
+    left_centers[0] =  (box1[0], round(box1[2]+box1[3]/2))
+    left_centers[1] =  (box2[0], round(box2[2]+box2[3]/2))
+    left_centers[2] =  (box3[0], round(box3[2]+box3[3]/2))
+    left_centers[3] =  (box4[0], round(box4[2]+box4[3]/2))
+
+    for i, box in enumerate(box_coordinates):
+        right_centers_box_full[i] = (box[0]+ box[1], round(box[2]+box[3]/2))
+        left_centers_box_full[i]  = (box[0], round(box[2]+ box[3]/2))
+    
+    return right_centers, left_centers, right_centers_box_full, left_centers_box_full
+
+
+def drawlineBetweenBox(BoxNum, right_centers, left_centers_box_full, box2_r_neighbours, img):
+    
+    start_point = int(right_centers[BoxNum][0]),int(right_centers[BoxNum][1])
+    box2_neighbour_indexes = np.squeeze(box2_r_neighbours)
+    end_point = int(left_centers_box_full[box2_neighbour_indexes][0]), int(left_centers_box_full[box2_neighbour_indexes][1])
+
+    return cv2.line(img, end_point,start_point,  (0,255,0), 3)
+
+def denoiseImage(img):
+    
+    img_denoise = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
+    imgGray = cv2.cvtColor(img_denoise , cv2.COLOR_BGR2GRAY)
+    ret, imgf = cv2.threshold(imgGray , 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) #imgf contains Binary image
+    
+    kernel = np.ones((3,3), np.uint8)
+    img_dilation = cv2.dilate( imgf, kernel, iterations=1)
+    img_erosion = cv2.erode(img_dilation , kernel, iterations=1)
+    
+    img_erosion = cv2.resize(img_erosion ,(img_erosion.shape[1]+100, img_erosion.shape[0]+50))
+    return img_erosion
+
+def detectFace(img):
+
+    h, w = img.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,
+    (300, 300), (104.0, 117.0, 123.0))
+    FaceNet.setInput(blob)
+    faces = FaceNet.forward()
+    
+    for i in range(faces.shape[2]):
+        confidence = faces[0, 0, i, 2]
+        if confidence > 0.6:
+            print("Confidence:", confidence)
+            return confidence
+        return 0
